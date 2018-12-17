073e6ff96788a78129098a7e7e2d7d7a9d1393b9
t24112541 <t24112541@gmail.com>
2018-12-17 15:24:36 +0700

    sm_plug api

---
+++ config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  port: 7005,
+  db: {
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'smart_plug'
+  },
+  socket: {
+    url: 'https://socket.bpcd.xenex.io',
+    user: 'bpcd',
+    pass: 'bpcd!@1234',
+  },
+}
