577cfabd4297f27393f32bd6a1fd2bae51bb980c
t24112541 <t24112541@gmail.com>
2018-12-24 03:26:10 +0700

    new config for docker oak

--- config.js
+++ config.js
@@ -2,10 +2,11 @@
 module.exports = {
   port: 7005,
   db: {
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'smart_plug',
+  	port:process.env.MYSQL_PORT || 3306,
+    host : process.env.MYSQL_HOST || 'localhost',
+    user : process.env.MYSQL_USER || 'root',
+    password : process.env.MYSQL_PASS || 'mysql1234',
+    database : process.env.MYSQL_DATABASE || 'smart_plug',
     timezone: 'asia/bangkok',
   },
   socket: {
