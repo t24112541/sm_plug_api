29179e25b227ef2378db8d6115bb0f563f861a70
t24112541 <t24112541@gmail.com>
2018-12-24 03:29:58 +0700

    nre config for oak 2

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 module.exports = {
   port: 7005,
   db: {
-  	port:process.env.MYSQL_PORT || 3306,
+  	port:process.env.MYSQL_PORT || 34001,
     host : process.env.MYSQL_HOST || 'localhost',
     user : process.env.MYSQL_USER || 'root',
     password : process.env.MYSQL_PASS || 'mysql1234',
-    database : process.env.MYSQL_DATABASE || 'smart_plug',
+    database : process.env.MYSQL_DATABASE || 'plugctc',
     timezone: 'asia/bangkok',
   },
   socket: {
